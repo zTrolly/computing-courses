a4a2d36be0452f219d5fd800c47f3d61071711b8
Breno Lopes do Carmo <61760048+zTrolly@users.noreply.github.com>
2022-06-25 21:55:39 -0300

    a

--- Frontend/PokeDex/pokedex/src/App.js
+++ Frontend/PokeDex/pokedex/src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemonName, setPokemonName] = useState('');
+  const [pokemonType, setPokemonType] = useState('');
+  const [pokemonWeight, setPokemonWeight] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  // tratar input
+  const handleChange = (event) => {
+    setTypedPokemon(event.target.value);
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Pokedex</h1>
+      <p>Digite o nome do pokemon para ser pesquisado...</p>
       </header>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          value={pokemonType}
+          placeholder="Digite o nome do pokemon"
+          onChange={handleChange}
+        />
+
+        <button type="submit">
+            {isLoading ? 'Carregando...' : 'Pesquisar'}
+            Pesquisar
+          </button>  
+      </form>
     </div>
   );
 }
