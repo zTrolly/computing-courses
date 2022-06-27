934220bb784e357f197774e3416184542684191f
Breno Lopes do Carmo <61760048+zTrolly@users.noreply.github.com>
2022-06-27 07:42:19 -0300

    teste

--- Frontend/PokeDex/pokedex/src/App.js
+++ Frontend/PokeDex/pokedex/src/App.js
@@ -3,41 +3,51 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  const [pokemon, setPokemon] = useState([]);
-  const [pokemonName, setPokemonName] = useState('');
-  const [pokemonType, setPokemonType] = useState('');
-  const [pokemonWeight, setPokemonWeight] = useState('');
-  const[error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  // const [pokemon, setPokemon] = useState([]);
+  // const [pokemonName, setPokemonName] = useState('');
+  // const [pokemonType, setPokemonType] = useState('');
+  // const [pokemonWeight, setPokemonWeight] = useState('');
+  // const[error, setError] = useState('');
+  // const [isLoading, setIsLoading] = useState(false);
 
 
-  // tratar input
-  const handleChange = (event) => {
-    setTypedPokemon(event.target.value);
-  };
+  // // tratar input
+  // const handleChange = (event) => {
+  //   setTypedPokemon(event.target.value);
+  // };
 
 
   return (
+
     <div className="App">
       <header className="App-header">
-      <h1>Pokedex</h1>
-      <p>Insert a Pokemon name for search</p>
+        
+    <h1>Teste do h1</h1>
       </header>
-
-      <form onSubmit={handleSubmit}>
-        <input
-          value={pokemonType}
-          placeholder="Write the pokemon's name"
-          onChange={handleChange}
-        />
-
-        <button type="submit">
-            {isLoading ? 'Loading...' : 'Search'}
-            Search
-          </button>  
-      </form>
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //   <h1>Pokedex</h1>
+    //   <p>Insert a Pokemon name for search</p>
+    //   </header>
+
+    //   <form onSubmit={handleSubmit}>
+    //     <input
+    //       value={pokemonType}
+    //       placeholder="Write the pokemon's name"
+    //       onChange={handleChange}
+    //     />
+
+    //     <button type="submit">
+    //         {isLoading ? 'Loading...' : 'Search'}
+    //         Search
+    //       </button>  
+    //   </form>
+    // </div>
   );
+
+
+
 }
 
 export default App;
