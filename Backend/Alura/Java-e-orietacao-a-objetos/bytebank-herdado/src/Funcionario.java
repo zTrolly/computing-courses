4987bb9b73fe69a2e9fe0e2991dd58afedd07642
Breno Lopes do Carmo <61760048+zTrolly@users.noreply.github.com>
2022-06-28 18:15:06 -0300

    first commit

---
+++ Backend/Alura/Java-e-orietacao-a-objetos/bytebank-herdado/src/Funcionario.java
@@ -0,0 +1,34 @@
+
+public class Funcionario {
+	
+	public Funcionario(String nome, double salario, String cpf) {
+		super();
+		this.nome = nome;
+		this.salario = salario;
+		this.cpf = cpf;
+	}
+	
+	private String nome;
+	private double salario;
+	private String cpf;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
